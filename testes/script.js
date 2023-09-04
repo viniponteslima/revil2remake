4b7a0a4747cccf3da71f0ed71f5cd7ccc84f2dfe
Vinícius Pontes Lima <33464603+viniponteslima@users.noreply.github.com>
2023-09-04 10:21:43 -0300

    Delete testes directory

--- testes/script.js
+++
@@ -1,7 +0,0 @@
-function changePage(next) {
-  var current = document.querySelector(".page.visible");
-  current.classList.remove("visible");
-  
-  var nextPage = document.getElementById(next);
-  nextPage.classList.add("visible");
-}
